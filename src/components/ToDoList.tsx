c7d330eb6d44c2451cf982f5adf8add41519ec83
webfirewoodja@gmail.com <jaehongkim@jaehongui-MacBookAir.local>
2022-03-23 12:58:06 +0900

    Add: eslint, prettier

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,34 +1,46 @@
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "../modules";
-import styled from "styled-components";
-import {BsTrash} from "react-icons/all";
-import {IToDo} from "../modules/todo/types";
-import {deleteTodo} from "../modules/todo/actions";
-import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../modules';
+import styled from 'styled-components';
+import { BsTrash, FiEdit } from 'react-icons/all';
+import { IToDo } from '../modules/todo/types';
+import { deleteTodo } from '../modules/todo/actions';
+import React from 'react';
 
 const ToDoWrapper = styled.div`
-  display: flex;
-  justify-content: space-between;
+    display: flex;
+    justify-content: space-between;
     border: 1px solid;
-  border-radius: 15px;
-  margin: 10px;
-  padding: 10px;
-  width: 50%;
+    border-radius: 15px;
+    margin: 10px;
+    padding: 10px;
+    width: 50%;
 `;
 
-const DeleteBtn = styled.button`
-
+const ButtonWrapper = styled.div`
+    svg {
+        padding-left: 10px;
+    }
 `;
 
 function ToDoList() {
-    let todoList = useSelector((state:RootState) => state.todo.todo);
+    let todoList = useSelector((state: RootState) => state.todo.todo);
     const dispatch = useDispatch();
     const onRemove = (item: IToDo) => {
         dispatch(deleteTodo(item));
-    }
+    };
 
-    return (<>
-        {todoList.map(item => <ToDoWrapper key={item.id}>{item.todo}<BsTrash onClick={() => onRemove(item)} /></ToDoWrapper>)}
-        </>)
+    return (
+        <>
+            {todoList.map(item => (
+                <ToDoWrapper key={item.id}>
+                    {item.todo}
+                    <ButtonWrapper>
+                        <FiEdit onClick={() => onRemove(item)} />
+                        <BsTrash onClick={() => onRemove(item)} />
+                    </ButtonWrapper>
+                </ToDoWrapper>
+            ))}
+        </>
+    );
 }
 export default ToDoList;
